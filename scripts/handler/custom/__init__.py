c851ac36d361df84dc891e2ba6d27d6b398d3107
layman <layman@localhost>
2016-09-20 12:53:34 +0300

    bump

--- scripts/handler/custom/__init__.py
+++ scripts/handler/custom/__init__.py
@@ -1,1 +1,1 @@
-from . import libsoup, tracker, vala, gobject_introspection, glib, gst_plugins_good, gst_plugins_ugly, gst_plugins_bad, gst_plugins_base
+from . import tracker, vala, gobject_introspection, glib, gst_plugins_good, gst_plugins_ugly, gst_plugins_bad, gst_plugins_base
