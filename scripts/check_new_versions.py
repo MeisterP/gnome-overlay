f3be87ba12cfb457b6c069066ec4b2baa0a4cd14
thankjura <me@slie.ru>
2017-09-11 13:50:15 +0300

    bump

--- scripts/check_new_versions.py
+++ scripts/check_new_versions.py
@@ -1,44 +1,73 @@
-#! env python2.7
+#! env python3
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+
+from datetime import datetime
+from time import sleep
 from os import path
+import asyncio
 from handler.version import LOCAL_PREFIX, get_last_ftp_version, get_last_local_version
 from handler.ebuild import create_ebuild
 from handler import custom
 
 custom_modules = [m for m in dir(custom) if not m.startswith('__')]
+loop = asyncio.get_event_loop()
 
 
-def main(conf):
+async def check_atom_process(atom, slot):
+    pkg_name = atom.split("/")[1]
+    last_local_version = sorted(get_last_local_version(atom))[-1]
+    print("local version: %s" % last_local_version.vstring)
+
+    custom_handler = False
+    only_local_check = False
+    if pkg_name.replace("-", "_") in custom_modules:
+        custom_handler = True
+        mod = getattr(custom, pkg_name.replace("-", "_"))
+        only_local_check = getattr(mod, 'ONLY_LOCAL_CHECK', False)
+
+    if only_local_check:
+        last_ftp_version = last_local_version
+    else:
+        last_ftp_version = await get_last_ftp_version(pkg_name, slot)
+        print("ftp version: %s" % last_ftp_version.vstring)
+        if last_ftp_version > last_local_version:
+            create_ebuild(atom, last_ftp_version)
+
+    print("check %s" % atom)
+
+    if custom_handler:
+        getattr(custom, pkg_name.replace("-", "_")).run(last_ftp_version)
+
+
+async def check_atom(atom):
+    e = atom.strip().split(":")
+    atom = e[0]
+    slot = e[1] if len(e) == 2 else None
+
+    x = asyncio.ensure_future(check_atom_process(atom, slot))
+    await x
+
+
+async def bound_check(sem, atom):
+    async with sem:
+        await check_atom(atom)
+
+
+async def main(conf):
+    sem = asyncio.Semaphore(4)
+    tasks = []
     for atom in conf:
-        e = atom.strip().split(":")
-        atom = e[0]
-        slot = e[1] if len(e) == 2 else None
+        task = asyncio.ensure_future(bound_check(sem, atom))
+        tasks.append(task)
 
-        print("check %s" % atom)
-        pkg_name = atom.split("/")[1]
-        last_local_version = sorted(get_last_local_version(atom))[-1]
-        print("local version: %s" % last_local_version.vstring)
-
-        custom_handler = False
-        only_local_check = False
-        if pkg_name.replace("-", "_") in custom_modules:
-            custom_handler = True
-            mod = getattr(custom, pkg_name.replace("-", "_"))
-            only_local_check = getattr(mod, 'ONLY_LOCAL_CHECK', False)
-
-        if only_local_check:
-            last_ftp_version = last_local_version
-        else:
-            last_ftp_version = get_last_ftp_version(pkg_name, slot)
-            print("ftp version: %s" % last_ftp_version.vstring)
-            if last_ftp_version > last_local_version:
-                create_ebuild(atom, last_ftp_version)
-
-        if custom_handler:
-            getattr(custom, pkg_name.replace("-", "_")).run(last_ftp_version)
+    responses = asyncio.gather(*tasks)
+    await responses
 
 
 if __name__ == '__main__':
+    start = datetime.now()
     with open(path.join(LOCAL_PREFIX, 'apps_list.conf')) as config:
-        main(config)
+        loop.run_until_complete(main(config))
+
+    print("Finish at {} sec".format((datetime.now() - start)))
--- scripts/handler/version.py
+++ scripts/handler/version.py
@@ -1,42 +1,45 @@
-from ftplib import FTP
+import aioftp
 from os import path, listdir
 from distutils.version import LooseVersion as Version
 from re import compile
 from pkg_resources import parse_version
 
 version_regexp = compile("-(\d+(\.\d+)*(-r\d)?)")
-ftp = FTP('ftp.gnome.org')
-ftp.login()
+host = 'ftp.gnome.org'
 
 PORTAGE_PREFIX = '/usr/portage/'
 FTP_PREFIX = '/pub/gnome/sources/'
 LOCAL_PREFIX = path.dirname(path.dirname(__file__))
 
 
-def get_last_ftp_version(atom, slot=None):
-    if slot:
-        slot = Version(slot)
-    ftp.cwd(FTP_PREFIX + atom)
-    slots = []
-    ftp.dir(lambda x: slots.append(Version(x.split()[-1])) if x.startswith('d') else None)
-    awailable_slots = sorted(slots)
-    if slot:
-        if slot in awailable_slots:
-            ftp.cwd(slot.vstring)
+async def get_last_ftp_version(atom, slot=None):
+    async with aioftp.ClientSession(host) as client:
+        if slot:
+            slot = Version(slot)
+        await client.change_directory(FTP_PREFIX + atom)
+        slots = []
+        for p, info in (await client.list()):
+            if info.get('type') == 'dir':
+                slots.append(Version(str(p)))
+        available_slots = sorted(slots)
+        if slot:
+            if slot in available_slots:
+                await client.change_directory(slot.vstring)
+            else:
+                last_slot = [s for s in available_slots if s <= slot][-1]
+                await client.change_directory(last_slot.vstring)
         else:
-            last_slot = [s for s in awailable_slots if s <= slot][-1]
-            ftp.cwd(last_slot.vstring)
-    else:
-        last_slot = awailable_slots[-1]
-        ftp.cwd(last_slot.vstring)
-    versions = []
-    ftp.dir(lambda x: versions.append(Version(version_regexp.findall(x.split()[-1])[0][0])) if x.endswith(
-        'tar.xz') else None)
-    return sorted(versions)[-1]
+            last_slot = available_slots[-1]
+            await client.change_directory(last_slot.vstring)
+        versions = []
+        for p, info in (await client.list()):
+            if str(p).endswith('tar.xz'):
+                versions.append(Version(version_regexp.findall(str(p))[0][0]))
+        return sorted(versions)[-1]
 
 
 def get_last_local_version(atom):
-    def get_last_version(prefix, atom):
+    def get_last_version(prefix):
         versions = []
         if not path.exists(path.join(prefix, atom)):
             return Version('0')
@@ -48,6 +51,6 @@ def get_last_local_version(atom):
             return sorted(versions)[-1]
         return Version('0')
 
-    last_portage_version = get_last_version(PORTAGE_PREFIX, atom)
-    last_overlay_version = get_last_version(path.dirname(LOCAL_PREFIX), atom)
+    last_portage_version = get_last_version(PORTAGE_PREFIX)
+    last_overlay_version = get_last_version(path.dirname(LOCAL_PREFIX))
     return last_overlay_version, last_portage_version
